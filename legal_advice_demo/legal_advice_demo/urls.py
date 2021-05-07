31203bb28127d36e878a3800eb5377851fe355d2
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-05-07 13:29:14 +0200

    Adjust to changes

--- legal_advice_demo/legal_advice_demo/urls.py
+++ legal_advice_demo/legal_advice_demo/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
+from .views import AnswerDetail
 from .views import AnswerDetailDownload
 from .views import LawCaseDetail
 from .views import LawCaseForm
@@ -11,5 +12,6 @@ urlpatterns = [
     path('', LawCaseList.as_view(), name='law-case-list'),
     path('<int:pk>/', LawCaseDetail.as_view(), name='law-case-detail'),
     path('<int:pk>/form/', LawCaseForm.as_view(), name='law-case-form'),
+    path('answers/<int:pk>/', AnswerDetail.as_view(), name='answer-detail-download'),
     path('answers/<int:pk>/download/', AnswerDetailDownload.as_view(), name='answer-detail-download')
 ]
--- legal_advice_demo/legal_advice_demo/views.py
+++ legal_advice_demo/legal_advice_demo/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import DetailView
 from django.views.generic import ListView
+from legal_advice_builder.mixins import GenerateEditableDocumentMixin
 from legal_advice_builder.models import Answer
 from legal_advice_builder.models import LawCase
 from legal_advice_builder.views import FormWizardView
@@ -16,7 +17,13 @@ class LawCaseDetail(DetailView):
     model = LawCase
 
 
-class LawCaseForm(FormWizardView):
+class AnswerDetail(DetailView):
+
+    model = Answer
+
+
+class LawCaseForm(GenerateEditableDocumentMixin,
+                  FormWizardView):
 
     def get_lawcase(self):
         pk = self.kwargs.get('pk')
