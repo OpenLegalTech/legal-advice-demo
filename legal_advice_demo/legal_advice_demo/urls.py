8f52c3deb9d6f6aa008fddb3e8f5e6efe6cbf2fc
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-06-21 12:57:06 +0200

    Remove Admin views from demo project

--- legal_advice_demo/legal_advice_demo/urls.py
+++ legal_advice_demo/legal_advice_demo/urls.py
@@ -4,8 +4,6 @@ from django.urls import path
 
 from .views import AnswerDetail
 from .views import AnswerUpdate
-from .views import DocumentEditAdminView
-from .views import DocumentPreviewAdminView
 from .views import LawCaseDetail
 from .views import LawCaseForm
 from .views import LawCaseList
@@ -17,9 +15,6 @@ urlpatterns = [
     path('<int:pk>/form/', LawCaseForm.as_view(), name='law-case-form'),
     path('answers/<int:pk>/', AnswerDetail.as_view(), name='answer-detail-download'),
     path('answers/<int:pk>/edit/', AnswerUpdate.as_view(), name='answer-detail-update'),
-    path('documents/<int:pk>/edit/', DocumentEditAdminView.as_view(), name='document-update-view'),
-    path('documents/<int:pk>/', DocumentPreviewAdminView.as_view(), name='document-detail-view'),
-    path('documents/', DocumentEditAdminView.as_view(), name='document-create-view'),
     path('tinymce/', include('tinymce.urls')),
     path('klageautomat/admin/', include('legal_advice_builder.urls'))
 ]
--- legal_advice_demo/legal_advice_demo/views.py
+++ legal_advice_demo/legal_advice_demo/views.py
@@ -4,10 +4,7 @@ from django.views.generic import ListView
 from django.views.generic import UpdateView
 from legal_advice_builder.forms import RenderedDocumentForm
 from legal_advice_builder.models import Answer
-from legal_advice_builder.models import Document
 from legal_advice_builder.models import LawCase
-from legal_advice_builder.views import DocumentFormView
-from legal_advice_builder.views import DocumentPreviewView
 from legal_advice_builder.views import FormWizardView
 
 
@@ -40,19 +37,3 @@ class LawCaseForm(FormWizardView):
     def get_lawcase(self):
         pk = self.kwargs.get('pk')
         return LawCase.objects.get(pk=pk)
-
-
-class DocumentPreviewAdminView(DocumentPreviewView):
-
-    def get_document(self):
-        if 'pk' in self.kwargs:
-            pk = self.kwargs.get('pk')
-            return Document.objects.get(pk=pk)
-
-
-class DocumentEditAdminView(DocumentFormView):
-
-    def get_document(self):
-        if 'pk' in self.kwargs:
-            pk = self.kwargs.get('pk')
-            return Document.objects.get(pk=pk)
