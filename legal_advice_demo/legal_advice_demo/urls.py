63ec3e62f8414954732da901af3658c5b37c64a4
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-08-23 16:42:47 +0200

    Add login urls and template

--- legal_advice_demo/legal_advice_demo/urls.py
+++ legal_advice_demo/legal_advice_demo/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include
 from django.urls import path
 
 urlpatterns = [
     path('', include('legal_advice_builder.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls)
 ]
